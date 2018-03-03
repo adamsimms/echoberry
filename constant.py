1c4383286394c6561a295e38c21323d49606be2c
RPi Guru <rpiguru@techie.com>
2018-03-03 16:05:03 +1000

    Fixed #5

--- constant.py
+++ constant.py
@@ -3,3 +3,6 @@ SWITCH_PIN = 4
 
 # relative path of the ring file in mp3 format
 RING_FILE = 'sounds/ring.mp3'
+FOREST_SOUND_FILE = 'sounds/forest_reduced15db.mp3'
+
+ICECAST_PASSWORD = 'MyAdminPassword'
--- main.py
+++ main.py
@@ -13,8 +13,8 @@ from constant import *
 cur_dir = os.path.dirname(os.path.realpath(__file__))
 ring_path = os.path.join(cur_dir, RING_FILE)
 
-CMD = 'ffmpeg -ac 1 -re -f alsa -i hw:1,0 -re -i sounds/forest_1h.mp3 ' \
-      '-filter_complex amerge=inputs=2 -f mp3 icecast://source:MyAdminPassword@54.89.215.33:8000/echoberry-yul'
+CMD = 'ffmpeg -ac 1 -re -f alsa -i hw:1,0 -re -i {} -filter_complex amerge=inputs=2 -f mp3 ' \
+      'icecast://source:{}@54.89.215.33:8000/echoberry-yul'.format(FOREST_SOUND_FILE, ICECAST_PASSWORD)
 
 
 class RPiEcho(threading.Thread):
@@ -45,7 +45,7 @@ class RPiEcho(threading.Thread):
         print('{}:: Switch is closed.'.format(datetime.datetime.now()))
         self.state = 'idle'
         kill_process_by_name('omxplayer')
-        kill_process_by_name('darkice')
+        kill_process_by_name('ffmpeg')
         kill_process_by_name('mplayer')
 
 
