3abc0cdde8b48630ea80f0017200e6995db49a3e
RPi Guru <rpiguru@techie.com>
2018-03-04 11:58:09 +1000

    Added installation script of ffmpeg for #9

---
+++ install_ffmpeg.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+echo "Installing FFMPEG now..."
+
+cd /tmp
+git clone git://git.videolan.org/x264
+cd x264
+./configure --host=arm-unknown-linux-gnueabi --enable-static --disable-opencl
+make
+sudo make install
+
+sudo apt-get install libmp3lame-dev
+
+cd /tmp
+git clone https://github.com/FFmpeg/FFmpeg.git
+cd FFmpeg
+./configure --arch=armel --target-os=linux --enable-gpl --enable-libx264 --enable-nonfree --enable-libmp3lame
+make -j4
+sudo make install
